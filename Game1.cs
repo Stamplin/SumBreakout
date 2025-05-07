8c81ef90e40f1f6bc4af6a29ede9b27a17862976
SB <701017313@GoTVDSB.ca>
2025-05-07 14:47:18 -0400

    more feauture just need skin and textures now

--- Game1.cs
+++ Game1.cs
@@ -41,6 +41,21 @@ namespace SumBreakout
             return false;
         }
 
+        //random
+        Random random = new Random();
+
+
+        //brick properties 
+        int brickActualWidth = 80;
+        int brickActualHeight = 20;
+        int desiredHorizontalGap = 7;
+        int desiredVerticalGap = 5;
+        int numberOfColumns = 9;
+        int numberOfRows = 5;
+        int leftScreenMargin = 10;
+        int topScreenMargin = 10;
+
+       
 
 
 
@@ -63,20 +78,24 @@ namespace SumBreakout
             //rectangles
             ballRect = new Rectangle(0, 0, 40, 40);
 
-            paddleRect = new Rectangle(0, 0, 228/2, 46/2);
+            paddleRect = new Rectangle(0, 0, 228 / 2, 46 / 2);
 
             brickRect = new Rectangle(0, 0, 50, 20);
 
             //spawn bricks in grid with gap between
-            for (int i = 0; i < 10; i++)
+
+            for (int i = 0; i < numberOfColumns; i++)
             {
-                for (int j = 0; j < 10; j++)//rows
+                for (int j = 0; j < numberOfRows; j++)
                 {
-                    int x = i * (80 + 11);//width
-                    int y = j * (20 + 5); //height
-                    bricks.Add(new Rectangle(x, y, 75, 20));
+                    int x = leftScreenMargin + i * (brickActualWidth + desiredHorizontalGap);
+                    int y = topScreenMargin + j * (brickActualHeight + desiredVerticalGap);
+                    bricks.Add(new Rectangle(x, y, brickActualWidth, brickActualHeight));
                 }
             }
+
+            //keystate
+            keyboardState = Keyboard.GetState();
 
 
 
@@ -105,6 +124,10 @@ namespace SumBreakout
             //mouse state
             mouseState = Mouse.GetState();
 
+            //keystate
+            PreviousKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+
             //paddle logic paddle get mouse pos and follow
             paddleRect.X = mouseState.X - paddleRect.Width / 2;
             paddleRect.Y = 550;
@@ -130,7 +153,7 @@ namespace SumBreakout
                 ballRect.X = paddleRect.X + paddleRect.Width / 2 - ballRect.Width / 2;
                 ballRect.Y = paddleRect.Y - ballRect.Height;
             }
-            
+
 
             //if space is press game start
             if (Keyboard.GetState().IsKeyDown(Keys.Space))
@@ -148,7 +171,16 @@ namespace SumBreakout
                     ballRect.X = paddleRect.X + paddleRect.Width / 2 - ballRect.Width / 2;
                     ballRect.Y = paddleRect.Y - ballRect.Height;
 
-                    ballSpeedX = 5;
+
+                    //random direction
+                    if (random.NextDouble() < 0.5)
+                    {
+                        ballSpeedX = -5;
+                    }
+                    else
+                    {
+                        ballSpeedX = 5;
+                    }
                     ballSpeedY = -5;
                 }
 
@@ -170,6 +202,8 @@ namespace SumBreakout
                 if (ballRect.Intersects(paddleRect))
                 {
                     ballSpeedY = -ballSpeedY;
+                    //fix
+                    ballRect.Y = paddleRect.Y - ballRect.Height;
                 }
 
                 //if ball hits brick, destroy it
@@ -179,8 +213,23 @@ namespace SumBreakout
                     {
                         ballSpeedY = -ballSpeedY;
                         bricks.RemoveAt(i);
+
+                        ballSpeedX *= 1.02f;
+                        ballSpeedY *= 1.02f;
                     }
                 }
+            }
+
+            //window title is current speed
+            Window.Title = ballSpeedX.ToString() + ", " + ballSpeedY.ToString();
+            //speed limit
+            if (ballSpeedX > 11)
+            {
+                ballSpeedX = 10;
+            }
+            if (ballSpeedY > 11)
+            {
+                ballSpeedY = 10;
             }
 
 
@@ -190,6 +239,8 @@ namespace SumBreakout
                 gameStart = false;
             }
 
+         
+           
 
 
 
